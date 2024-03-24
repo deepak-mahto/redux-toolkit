c64925e1916f1d098e4ee0bdc453b4f51567f75c
Deepak Kumar <mahtodeepak857@gmail.com>
2024-03-24 05:38:44 +0530

    Styling

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -7,14 +7,14 @@ const Todos = () => {
   const dispatch = useDispatch();
   return (
     <>
-      <div>Todos</div>
+      <h1>Todos</h1>
       <ul className="list-none">
         {todos.map((todo) => (
           <li
-            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            className="mt-4 flex justify-between items-center bg-gray-300 px-4 py-2 rounded"
             key={todo.id}
           >
-            <div className="text-white">{todo.text}</div>
+            <div className="text-black">{todo.text}</div>
             <button
               onClick={() => dispatch(removeTodo(todo.id))}
               className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
